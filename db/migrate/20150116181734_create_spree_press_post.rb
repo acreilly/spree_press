36c1ec037aa68432e3c6df2ac05dfd1988266ef6
acreilly <acreilly3@gmail.com>
2015-01-16 14:31:05 -0500

    you can add press posts with images

--- db/migrate/20150116181734_create_spree_press_post.rb
+++ db/migrate/20150116181734_create_spree_press_post.rb
@@ -1,6 +1,6 @@
-class CreatePressPost < ActiveRecord::Migration
+class CreateSpreePressPost < ActiveRecord::Migration
   def change
-    create_table :press_posts do |t|
+    create_table :spree_press_posts do |t|
       t.column :title, :string
       t.column :body, :text
       t.timestamps
