0f08399a8f218b9fa408c6a17cfd77a5f3683c94
acreilly <acreilly3@gmail.com>
2015-01-19 16:55:10 -0500

    added pagination

--- app/controllers/spree/press_posts_controller.rb
+++ app/controllers/spree/press_posts_controller.rb
@@ -1,6 +1,13 @@
 class Spree::PressPostsController < Spree::StoreController
-
+  before_filter :init_pagination, :only => [:index]
   def index
-    @press_posts = Spree::PressPost.all
+    @press_posts = Spree::PressPost.page(@pagination_page).per(@pagination_per_page)
   end
+
+   private
+
+    def init_pagination
+      @pagination_page = params[:page].to_i > 0 ? params[:page].to_i : 1
+      @pagination_per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 10
+    end
 end
